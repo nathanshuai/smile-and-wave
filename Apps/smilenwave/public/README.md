e715d10f63054e998e7a64dcff74fc6725447afd
nathanshuai <nathan.shuai@gmail.com>
2023-05-10 11:22:47 -0500

    Configue react app

---
+++ Apps/smilenwave/public/README.md
@@ -0,0 +1,8 @@
+
+### Smile N Wave
+
+# Click [here]() to check the magic of smile and wave.
+
+<br />
+
+<img src="https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB" height="65">
---
+++ Apps/smilenwave/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+      <div className='container'>
+        <p>&copy; 2023 Smile N Wave. All Rights Reserved.</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
